766da1c7d96b51ce4fa1ca92cbdff260168b5b2f
qazimusab <musab.ahmed@gmail.com>
2016-04-04 11:05:03 -0400

    latest :)

--- Vault/Models/Comptadora.cs
+++ Vault/Models/Comptadora.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Vault.Models
 {
     public class Comptadora
     {
+        public Comptadora()
+        {
+            
+        }
+        public Comptadora(Computer aspNetComputer)
+        {
+            ComputerName = aspNetComputer.ComputerName;
+            DepartmentName = aspNetComputer.DepartmentName;
+            Credentials = aspNetComputer.Credentials.Select(aspNetCredential => new Credencial(aspNetCredential)).ToList();
+        }
+
+     
+
         public string ComputerName { get; set; }
+        public string DepartmentName { get; set; }
         public List<Credencial> Credentials { set; get; }
     }
 }
--- Vault/Models/Credencial.cs
+++ Vault/Models/Credencial.cs
@@ -2,7 +2,18 @@
 {
     public class Credencial
     {
+        public Credencial()
+        {
+            
+        }
+        public Credencial(Credential aspNetCredential)
+        {
+            UserName = aspNetCredential.UserName;
+            Password = aspNetCredential.Password;
+            Type = aspNetCredential.Type;
+        }
         public string UserName { get; set;}
         public string Password { get; set;}
+        public string Type { get; set;}
 }
 }
